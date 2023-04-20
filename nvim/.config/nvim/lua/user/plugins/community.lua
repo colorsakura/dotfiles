ba7e3c2d75b680c188d95bb21e221392fd87dd3c
iFlygo <2268136725@qq.com>
2023-04-20 15:53:25 +0800

    fix(nvim): remove astro community nix configuration;

--- nvim/.config/nvim/lua/user/plugins/community.lua
+++ nvim/.config/nvim/lua/user/plugins/community.lua
@@ -25,7 +25,6 @@ return {
         { import = "astrocommunity.pack.json" },
         { import = "astrocommunity.pack.lua" },
         { import = "astrocommunity.pack.markdown" },
-        { import = "astrocommunity.pack.nix" },
         { import = "astrocommunity.pack.python" },
         { import = "astrocommunity.pack.rust" },
         { import = "astrocommunity.pack.yaml" },
