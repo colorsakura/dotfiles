1fdfa2768b9660dbb4656899a7ad02fa8bf528fb
iFlygo <2268136725@qq.com>
2023-06-11 08:19:04 +0800

    feat(wm): add sway config and adjust waybar settings;

--- wm/waybar/scripts/recorder.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/env fish
-
-if killall -s SIGINT wf-recorder
-  notify-send "Recorder Finish"
-else
-  echo 100 > $WOBSOCK 
-  wf-recorder -f ~/$(date +%y-%m-%d-%k%M).mp4 -c hevc_vaapi -d /dev/dri/renderD128
-end
