3dff8ec020ce345442a1c358d346d71df4c8ef86
iFlygo <iflygo@outlook.com>
2023-07-13 22:11:50 +0800

    chore(fish): format fish config;
    Signed-off-by: iFlygo <iflygo@outlook.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
-# fish config
+# Fish config
 
-# from https://github.com/trapd00r/LS_COLORS
+# From https://github.com/trapd00r/LS_COLORS
 source ~/.config/fish/lscolors.csh
 
 set -U fish_emoji_width 2
@@ -18,48 +18,48 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_STATE_HOME $HOME/.local/state
 
+set -x GOPATH "$XDG_DATA_HOME"/go
 set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
 set -x RUSTUP_HOME "$XDG_DATA_HOME"/rustup
 set -x IPYTHONDIR "$XDG_CONFIG_HOME"/jupyter
 set -x JUPYTER_CONFIG_DIR "$XDG_CONFIG_HOME"/jupyter
-set -x GOPATH "$XDG_DATA_HOME"/go
 
 [ -d "$XDG_DATA_HOME"/gnupg ] || mkdir -m 700 -p "$XDG_DATA_HOME/gnupg"
 set -x GNUPGHOME "$XDG_DATA_HOME"/gnupg
 
+fish_add_path $GOPATH/bin
 fish_add_path $CARGO_HOME/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $XDG_CONFIG_HOME/rofi/scripts
-fish_add_path $GOPATH/bin
 
 set -x PYTHONPATH ~/.local/lib/python3.11/site-packages
 set -x PYTHONPATH /usr/lib/python3.11/site-packages $PYTHONPATH
 
 # Fcitx5
+set -x GLFW_IM_MODULE fcitx
 set -x GTK_IM_MODULE fcitx
+set -x INPUT_METHOD fcitx
 set -x QT_IM_MODULE fcitx
 set -x SDL_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
-set -x INPUT_METHOD fcitx
-set -x GLFW_IM_MODULE fcitx
 
 # Wayland
-set -x WINIT_UNIX_BACKEND wayland
-set -x QT_QPA_PLATFORM wayland
-set -x CLUTTER_BACKEND wayland
-set -x SDL_VIDEODRIVER wayland
-
-set -x MOZ_ENABLE_WAYLAND 1      # Firefox wayland
-
-set -x XDG_SESSION_TYPE wayland
-
 set -x ANKI_WAYLAND 1            # Anki wayland
+set -x CLUTTER_BACKEND wayland
+set -x MOZ_ENABLE_WAYLAND 1      # Firefox wayland
+set -x QT_QPA_PLATFORM wayland
+set -x SDL_VIDEODRIVER wayland
+set -x WINIT_UNIX_BACKEND wayland
+set -x XDG_SESSION_TYPE wayland
 
 # ISSUE: fix miss cuda for mpv
 # https://wiki.archlinux.org/title/Hardware_video_acceleration#Configuring_VA-API
 set -x LIBVA_DRIVER_NAME iHD
 set -x VDPAU_DRIVER va_gl
 
+# rustup bootstrap
+set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 
 set -x LIBVIRT_DEFAULT_URI "qemu:///system"
 # set -x GNOME_DESKTOP_SESSION_ID 0
@@ -67,18 +67,18 @@ set -x LIBVIRT_DEFAULT_URI "qemu:///system"
 set -x LC_CTYPE en_US.UTF-8
 
 set -x WOBSOCK $XDG_RUNTIME_DIR/wob.sock
-set -x CLION_VM_OPTIONS $HOME/dotfiles/utils/scripts/jetbra/vmoptions/clion.vmoptions
+# set -x CLION_VM_OPTIONS $HOME/dotfiles/utils/scripts/jetbra/vmoptions/clion.vmoptions
 
 # Alias
-alias v="nvim"
 alias c="curl"
-alias h="tldr"
 alias g="git"
 alias ga="git add"
 alias gc="git commit"
 alias gs="git status"
+alias h="tldr"
 alias sudo='sudo -E '
 alias tree="tree -a -I .git --dirsfirst"
+alias v="nvim"
 
 # 切记在设置环境变量后运行
 if status --is-login
