c49cade39a9f813339247b9b5fa4b58abec2abf8
iFlygo <iflygo@outlook.com>
2023-08-21 15:48:55 +0800

    chore(nvim): F5 set to open h term;
    Signed-off-by: iFlygo <iflygo@outlook.com>

--- config/nvim/lua/user/plugins/core.lua
+++ config/nvim/lua/user/plugins/core.lua
@@ -3,10 +3,16 @@ return {
         "L3MON4D3/LuaSnip",
         dependencies = { "rafamadriz/friendly-snippets" },
         config = function(plugin, opts)
-            require "plugins.configs.luasnip" (plugin, opts)
+            require "plugins.configs.luasnip"(plugin, opts)
             -- Add more custom luasnip configuration such as filetype extend or custom snippets
             local luasnip = require "luasnip.loaders.from_vscode"
             luasnip.lazy_load { paths = { "./lua/user/snippets" } }
         end,
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        opts = {
+            direction = "horizontal",
+        },
     },
 }
