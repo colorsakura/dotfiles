3f91b95c39d4163b5815077151825bb9aa782fc9
iFlygo <iflygo@outlook.com>
2023-08-23 08:16:55 +0800

    chore(nvim): remove duplicate plugin;
    Signed-off-by: iFlygo <iflygo@outlook.com>

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -6,15 +6,12 @@ return {
     python3_host_prog = "/usr/bin/python3",
     python_host_skip_check = 1,
 
-    -- copyright plugin
-    file_copyright_email = "iflygo@outlook.com",
-    file_copyright_name = "iFlygo",
     -- neovide
     neovide_input_ime = true,
   },
   opt = {
     filetype = "on",
-    modelines = 1,
+    cmdheight = 0,
 
     -- neovide
     guifont = "monospace",
--- config/nvim/lua/user/plugins/user.lua
+++ config/nvim/lua/user/plugins/user.lua
@@ -3,19 +3,4 @@ return {
     -- Add plugins, the lazy syntax
     { "lilydjwg/fcitx.vim", event = "InsertEnter" },
     { "yianwillis/vimcdoc", event = "UIEnter" },
-    {
-        "NvChad/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup {
-                user_default_options = {
-                    RRGGBBAA = true, -- #RRGGBBAA hex codes
-                    AARRGGBB = false, -- 0xAARRGGBB hex codes
-                    rgb_fn = true, -- CSS rgb() and rgba() functions
-                    hsl_fn = false, -- CSS hsl() and hsla() functions
-                    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-                },
-            }
-        end,
-    },
 }
