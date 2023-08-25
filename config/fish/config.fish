6d8bd1132d63589a81566fc982031c3a4ccf5fec
iFlygo <iflygo@outlook.com>
2023-08-25 15:43:27 +0800

    chore(fish): fix typo;
    Signed-off-by: iFlygo <iflygo@outlook.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,7 +3,7 @@
 # From https://github.com/trapd00r/LS_COLORS
 source ~/.config/fish/lscolors.csh
 
-# enable vi mode
+# Enable vi mode
 # fish_vi_key_bindings
 
 set -U fish_emoji_width 2
@@ -39,19 +39,19 @@ fish_add_path $HOME/.local/bin
 fish_add_path $XDG_CONFIG_HOME/rofi/scripts
 fish_add_path $XDG_DATA_HOME/gem/ruby/3.0.0/bin
 
-# set PYTHONPATH for neovim
+# set python3.11 PYTHONPATH for neovim
 set -x PYTHONPATH ~/.local/lib/python3.11/site-packages
 set -x PYTHONPATH /usr/lib/python3.11/site-packages $PYTHONPATH
 
 # Fcitx5
-set -x GLFW_IM_MODULE fcitx
-set -x GTK_IM_MODULE fcitx
+set -x GLFW_IM_MODULE fcitx # ibus|fcitx
+set -x GTK_IM_MODULE fcitx # wayland|fcitx
 set -x INPUT_METHOD fcitx
 set -x QT_IM_MODULE fcitx
 set -x SDL_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
 
-# Wayland Applications
+# Enable Wayland
 set -x ANKI_WAYLAND 1            # Anki wayland
 set -x CLUTTER_BACKEND wayland
 set -x MOZ_ENABLE_WAYLAND 1      # Firefox wayland
@@ -62,24 +62,25 @@ set -x XDG_SESSION_TYPE wayland
 
 # ISSUE: fix miss cuda for mpv
 # https://wiki.archlinux.org/title/Hardware_video_acceleration#Configuring_VA-API
-set -x LIBVA_DRIVER_NAME iHD
-set -x VDPAU_DRIVER va_gl
+# set -x LIBVA_DRIVER_NAME iHD
+# set -x VDPAU_DRIVER va_gl
 
 # rustup bootstrap
 set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 
-set -x LIBVIRT_DEFAULT_URI "qemu:///system"
+# set -x LIBVIRT_DEFAULT_URI "qemu:///system"
 # set -x GNOME_DESKTOP_SESSION_ID 0
 # set -x LC_CTYPE zh_CN.UTF-8
+
 # use en_US for fontconfig
 set -x LC_CTYPE en_US.UTF-8
 
 # wob
 set -x WOBSOCK $XDG_RUNTIME_DIR/wob.sock
+
 # Jetbrains APPS plugin
 source ~/.jetbrains.vmoptions.sh
-# set -x CLION_VM_OPTIONS $HOME/.local/bin/jetbra/vmoptions/clion.vmoptions
 
 # Alias
 alias c="curl"
@@ -95,8 +96,8 @@ alias v="nvim"
 # after login in tty, auto run wm;
 if status --is-login
    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-      set WLR_RENDERER vulkan
       set -x XDG_CURRENT_DESKTOP sway
+      set WLR_RENDERER vulkan
       exec sway 2> /tmp/startx.log || true
    end
    if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
