a9e412626b249d05160dd80730a509ecc5ccb7d7
iFlygo <iflygo@outlook.com>
2023-09-08 04:51:43 +0800

    nvim: remove python host config;

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -3,8 +3,8 @@ return {
     encoding = "utf-8",
     mapleader = " ",
 
-    python3_host_prog = "/usr/bin/python3",
-    python_host_skip_check = 1,
+    -- python3_host_prog = "/usr/bin/python3",
+    -- python_host_skip_check = 1,
 
     -- neovide
     neovide_input_ime = true,
