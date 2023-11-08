4614dc1e5826bfcc8d7da42ddd7eb8a738da64f2
iFlygo <iflygo@outlook.com>
2023-11-08 18:14:17 +0800

    fish: add inshellisense command

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,13 +3,13 @@
 # From https://github.com/trapd00r/LS_COLORS
 # curl -O https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/lscolors.csh
 if [ -f "$HOME/.config/fish/lscolors.csh" ]
-   source ~/.config/fish/lscolors.csh
+    source ~/.config/fish/lscolors.csh
 end
 
 # Disable greeting
 set -U fish_greeting ""
 
-set -U fish_emoji_width 2
+# set -U fish_emoji_width 2
 
 # Editor
 set -gx EDITOR (which nvim)
@@ -32,18 +32,18 @@ set -x GNUPGHOME "$XDG_DATA_HOME"/gnupg
 
 # Golang
 set -x GOPATH "$XDG_DATA_HOME"/go
-
 # Rust
 set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
 set -x RUSTUP_HOME "$XDG_DATA_HOME"/rustup
-
+# rustup bootstrap
+set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 # Python
 set -x IPYTHONDIR "$XDG_CONFIG_HOME"/jupyter
 set -x JUPYTER_CONFIG_DIR "$XDG_CONFIG_HOME"/jupyter
 # set python3.11 PYTHONPATH for neovim
 set -x PYTHONPATH ~/.local/lib/python3.11/site-packages
 set -x PYTHONPATH /usr/lib/python3.11/site-packages $PYTHONPATH
-
 # Ruby
 set -x BUNDLE_PATH $XDG_DATA_HOME/bundle
 
@@ -57,7 +57,7 @@ fish_add_path $XDG_DATA_HOME/gem/ruby/3.0.0/bin
 fish_add_path $HOME/.local/bin/evil-software
 
 # Fcitx5
-set -x GLFW_IM_MODULE fcitx # ibus|fcitx
+set -x GLFW_IM_MODULE ibus # ibus|fcitx
 set -x GTK_IM_MODULE fcitx # wayland|fcitx
 set -x INPUT_METHOD fcitx
 set -x QT_IM_MODULE fcitx
@@ -65,26 +65,15 @@ set -x SDL_IM_MODULE fcitx
 set -x XMODIFIERS @im=fcitx
 
 # Enable Wayland
-set -x ANKI_WAYLAND 1            # Anki wayland
-set -x CLUTTER_BACKEND wayland
-set -x MOZ_ENABLE_WAYLAND 1      # Firefox wayland
-set -x QT_QPA_PLATFORM wayland
-set -x SDL_VIDEODRIVER wayland
-set -x WINIT_UNIX_BACKEND wayland
 set -x XDG_SESSION_TYPE wayland
-
-# ISSUE: fix miss cuda for mpv
-# https://wiki.archlinux.org/title/Hardware_video_acceleration#Configuring_VA-API
-# set -x LIBVA_DRIVER_NAME iHD
-# set -x VDPAU_DRIVER va_gl
-
-# rustup bootstrap
-set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
-set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
-
-# set -x LIBVIRT_DEFAULT_URI "qemu:///system"
-# set -x GNOME_DESKTOP_SESSION_ID 0
-# set -x LC_CTYPE zh_CN.UTF-8
+if [ "$XDG_SESSION_TYPE" = wayland ]
+    set -x ANKI_WAYLAND 1 # Anki wayland
+    set -x CLUTTER_BACKEND wayland
+    set -x MOZ_ENABLE_WAYLAND 1 # Firefox wayland
+    set -x QT_QPA_PLATFORM wayland
+    set -x SDL_VIDEODRIVER wayland
+    set -x WINIT_UNIX_BACKEND wayland
+end
 
 # use en_US for fontconfig
 set -x LC_CTYPE en_US.UTF-8
@@ -94,37 +83,44 @@ set -x WOBSOCK $XDG_RUNTIME_DIR/wob.sock
 
 # Jetbrains APPS plugin
 if [ -f "$HOME/.jetbrains.vmoptions.sh" ]
-   source ~/.jetbrains.vmoptions.sh
+    source ~/.jetbrains.vmoptions.sh
 end
 
 # Alias
-alias grep="rg"
+# alias grep="rg"
 # alias c="curl"
 # alias g="git"
-# alias ga="git add"
-# alias gc="git commit"
-# alias gs="git status"
+alias hx="helix"
+alias ga="git add"
+alias gc="git commit -s"
+alias gs="git status"
 # alias h="tldr"
 # alias tree="tree -a -I .git --dirsfirst"
 # alias v="nvim"
-alias ack ag
+# alias ack ag
 alias ls eza
 alias vim="nvim --clean"
 
 # 切记在设置环境变量后运行
 # after login in tty, auto run wm;
 if status --is-login
-   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-      if which sway > /dev/null 2>&1
-         set -x XDG_CURRENT_DESKTOP sway
-         set WLR_RENDERER vulkan
-         exec sway 2> /tmp/startx.log || true
-      end
-   end
-   if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
-      if which Hyprland > /dev/null 2>&1
-         set -x XDG_SESSION_DESKTOP hyprland
-         exec Hyprland 2> /tmp/startx.log || true
-      end
-   end
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        if which sway >/dev/null 2>&1
+            set -x XDG_CURRENT_DESKTOP sway
+            set -x WLR_RENDERER vulkan
+            exec sway 2>/tmp/startx.log || true
+        end
+    end
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
+        if which Hyprland >/dev/null 2>&1
+            set -x XDG_SESSION_DESKTOP hyprland
+            exec Hyprland 2>/tmp/startx.log || true
+        end
+    end
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 3
+        if which wayfire >/dev/null 2>&1
+            set -x XDG_SESSION_DESKTOP wayfire
+            exec wayfire 2>/tmp/startx.log || true
+        end
+    end
 end
