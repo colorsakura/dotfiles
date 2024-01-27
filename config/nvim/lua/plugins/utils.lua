6c0591bc1d5b7926cf94cad38cb43ce0e5f31dfb
iFlygo <iflygo@outlook.com>
2024-01-27 11:53:08 +0800

    fontconfig: set Dejavu to default english fonts
    - fix zhudou sans config

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -3,4 +3,10 @@ return {
     "lilydjwg/fcitx.vim",
     event = "InsertEnter",
   },
+  {
+    "b0o/incline.nvim",
+    opts = {},
+    -- Optional: Lazy load Incline
+    event = "VeryLazy",
+  },
 }
