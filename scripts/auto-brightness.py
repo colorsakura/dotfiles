c2abfd76f1d1a962c52640f25c0479c27852de8e
Chen Xiang <iflygo@outlook.com>
2024-08-31 19:12:17 +0800

    script: remove useless scripts

--- scripts/auto-brightness.py
+++
@@ -1,4 +0,0 @@
-#!/bin/env python3
-
-if __name__ == "__main__":
-    pass
--- scripts/ide-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-
-IDE=("https://download.jetbrains.com.cn/cpp/CLion-2023.1.3.tar.gz")
-
-download() {
-  for url in $IDE; do
-    curl -L "${url}"
-  done
-}
