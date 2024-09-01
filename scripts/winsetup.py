c56d72c71a2c81154d6cf91157f40c83164fb03d
Chen Xiang <iflygo@outlook.com>
2024-09-01 14:21:28 +0800

    script: add windows setup script

--- scripts/winsetup.py
+++ scripts/winsetup.py
@@ -1,1 +1,43 @@
 import subprocess
+
+
+def load_conf(path):
+    content = []
+    try:
+        with open(path, mode='r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.startswith('#'):
+                    pass
+                else:
+                    content.append(line.strip())
+    except FileNotFoundError:
+        print(f"Configuration file not found at {path}")
+    print(content)
+    return content
+
+
+# TODO: 需要区分已安装和安装失败
+def run(pkg) -> bool:
+    print(f"Installing {pkg}")
+    return subprocess.run(["winget", "install", pkg]).returncode == 0
+
+
+def sumary(status):
+    pass
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Setup')
+    parser.add_argument('-c', '--config_file', default="windows.pkg", help='Configuration file')
+    args = parser.parse_args()
+
+    status ={}
+
+    pkgs = load_conf(args.config_file)
+    for pkg in pkgs:
+        status[pkg] = run(pkg)
+
+    print(status)
