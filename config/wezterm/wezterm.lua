e75e6daa3eaa95d235d0c982a2986cf8edc88081
iFlygo <iflygo@outlook.com>
2024-12-24 01:30:14 +0800

    chore(wezterm): use powershell in windows

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,4 +4,8 @@ local config = {
   font = wezterm.font('JetBrains Mono', {})
 }
 
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_prog = { 'powershell.exe' }
+end
+
 return config
