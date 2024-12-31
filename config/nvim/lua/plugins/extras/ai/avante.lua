032b5bf7a3c43e985119d1d1d83d0ad45307490b
iFlygo <iflygo@outlook.com>
2025-01-01 07:30:00 +0800

    chore: 修复avante在windows上的安装问题

--- config/nvim/lua/plugins/extras/ai/avante.lua
+++ config/nvim/lua/plugins/extras/ai/avante.lua
@@ -40,8 +40,12 @@ return {
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    build = function()
+      if Editor.is_win() then
+        return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+      end
+      return "make"
+    end,
     dependencies = {
       -- "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
